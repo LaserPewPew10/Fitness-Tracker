de69c57e6fcd8daebfd67be624dbfb479ca340cd
Justin Ramirez <ramirezbjustin@gmail.com>
2020-08-19 21:00:20 -0400

    looked intoroutes for the application

---
+++ server.js
@@ -0,0 +1,31 @@
+// DEPENDECIES
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// application middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// connecting to the database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Listen on port 3000
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}!`);
+});
